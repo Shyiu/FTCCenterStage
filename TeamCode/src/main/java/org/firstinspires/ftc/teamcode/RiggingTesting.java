87757472986816727a0ee223349be8db1f91ba32
Rishthegreat <76021793+Rishthegreat@users.noreply.github.com>
2023-10-22 21:30:10 -0400

    chganfing riggig

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RiggingTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RiggingTesting.java
@@ -1,15 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subclasses.Rigging;
 
+@TeleOp(name = "RiggingTest")
 public class RiggingTesting extends LinearOpMode {
     Rigging rigging;
     @Override
     public void runOpMode(){
         rigging = new Rigging(hardwareMap);
-
+        waitForStart();
         while(!isStopRequested() && opModeIsActive()){
             rigging.setRiggingPower(gamepad2.left_stick_y/-3.0);
             telemetry.addData("position", rigging.getRiggingPosition());
